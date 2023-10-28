b2a3c7910b40397b18f826e18388a18e93c1631b
Alex <57301498+davidkoo1@users.noreply.github.com>
2023-10-28 03:47:55 +0300

    User logIn

---
+++ PracticeProject/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PracticeProject.Data;
+using PracticeProject.Models;
+using PracticeProject.ViewModels;
+
+namespace PracticeProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly ApplicationDbContext _applicationDbContext;
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, ApplicationDbContext applicationDbContext)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _applicationDbContext = applicationDbContext;
+        }
+        //[HttpGet]
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if(!ModelState.IsValid)
+                return View(loginVM);
+
+            var user = await _userManager.FindByEmailAsync(loginVM.ConfirmData) ?? await _userManager.FindByNameAsync(loginVM.ConfirmData);
+            if(user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if(passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if(result.Succeeded) 
+                    {
+                        return RedirectToAction("Index", "Course");
+                    }
+                }
+                TempData["Error"] = "Password is incorrect. Please try again";
+                return View(loginVM);
+            }
+            TempData["Error"] = "Email or username is incorrect";//"Wrong credetionals. Please try again";
+            return View(loginVM);
+        }
+    }
+}
---
+++ PracticeProject/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticeProject.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email Address or username")]
+        [Required(ErrorMessage = "Email or username is required")]
+        public string ConfirmData { get; set; } //UserName, Email
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set;}
+    }
+}
